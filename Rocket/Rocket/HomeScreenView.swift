2c97fbe3b3191e3b881d29d9318fab6d08317804
usedgenes <67665540+usedgenes@users.noreply.github.com>
2024-08-25 16:31:42 -0700

    Edited Files

--- Rocket/Rocket/HomeScreenView.swift
+++ Rocket/Rocket/HomeScreenView.swift
@@ -7,229 +7,156 @@ struct HomeScreenView: View {
     @State var getLoopTime = false
     
     var body: some View {
-        ScrollView {
+        NavigationView {
             Text("TVC Control Panel")
                 .font(.largeTitle)
                 .fontWeight(.bold)
                 .frame(alignment: .leading)
                 .offset(y: 30)
                 .padding()
-            Divider()
-            HStack {
-                    Button(action: {
-                        if(!getLoopTime) {
-                            bluetoothDevice.setUtilities(input: "Time Get")
-                        }
-                        else {
-                            bluetoothDevice.setUtilities(input: "Time Stop")
-                        }
-                        getLoopTime.toggle()
-                    }) {
-                        Text("Refresh Rate: ")
-                            .foregroundColor(.black)
-                    }.padding(.leading)
-                    Text(rocket.deltaTime + " ms")
-                Spacer()
-                Text(bluetoothDevice.isConnected ? "Connected" : "Not Connected")
-                    .foregroundColor(bluetoothDevice.isConnected ? .green : .red)
-                    .padding()
-            }
-            Divider()
-            HStack {
-                Button(action: {
-                    bluetoothDevice.setUtilities(input: "Arm")
-                }) {
-                    Text("Arm Rocket")
-                        .font(.title2)
-                }
-                .padding(.horizontal, 25)
-                Button(action: {
-                    bluetoothDevice.setUtilities(input: "Reset")
-                    rocket.reset()
-                }) {
-                    Text("Reset Rocket")
-                        .font(.title2)
-                }
-                .padding()
-            }
-            Divider()
-            Section {
-                Section {
-                    HStack {
-                        NavigationLink {
-                            BluetoothConnectView()
-                        } label: {
-                            Text("Connect Bluetooth:")
+            List {
+                Section(header: Text("Rocket Options")) {
+                    NavigationLink("Connect to Bluetooth:", destination: BluetoothConnectView())
+                    NavigationLink("View Orientation: ", destination: OrientationView())
+                    NavigationLink("View BMI088:", destination: BMI088View())
+                    NavigationLink("View Altimeter:", destination: AltimeterView())
+                    NavigationLink("View Servos:", destination: ServoView())
+                    NavigationLink("View PID:", destination: PIDView())
+                    Section(header: Text("Options")) {
+                        HStack {
+                            Button(action: {
+                                if(!getLoopTime) {
+                                    bluetoothDevice.setUtilities(input: "Time Get")
+                                }
+                                else {
+                                    bluetoothDevice.setUtilities(input: "Time Stop")
+                                }
+                                getLoopTime.toggle()
+                            }) {
+                                Text("Refresh Rate: ")
+                                    .foregroundColor(.black)
+                            }.padding(.leading)
+                            Text(rocket.deltaTime + " ms")
+                            Spacer()
+                            Text(bluetoothDevice.isConnected ? "Connected" : "Not Connected")
+                                .foregroundColor(bluetoothDevice.isConnected ? .green : .red)
                                 .padding()
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                                .foregroundColor(.black)
                         }
-                        Spacer()
-                        Image(systemName: "chevron.forward")
+                        Divider()
+                        HStack {
+                            Button(action: {
+                                bluetoothDevice.setUtilities(input: "Arm")
+                            }) {
+                                Text("Arm Rocket")
+                                    .font(.title2)
+                            }
+                            .padding(.horizontal, 25)
+                            Button(action: {
+                                bluetoothDevice.setUtilities(input: "Reset")
+                                rocket.reset()
+                            }) {
+                                Text("Reset Rocket")
+                                    .font(.title2)
+                            }
                             .padding()
-                    }
-                    Divider()
-                    HStack {
-                        NavigationLink {
-                            OrientationView()
-                        } label: {
-                            Text("View Orientation:")
+                        }
+                        Divider()
+                        Section {
+                            HStack {
+                                Button(action: {
+                                    bluetoothDevice.setUtilities(input: "Bypass Pad")
+                                }) {
+                                    Text("On Pad")
+                                }
                                 .padding()
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                                .foregroundColor(.black)
-                        }
-                        Spacer()
-                        Image(systemName: "chevron.forward")
-                            .padding()
-                    }
-                    Divider()
-                    HStack {
-                        NavigationLink {
-                            BMI088View()
-                        } label: {
-                            Text("View BMI088:")
+                                .buttonStyle(.borderless)
+                                Image(systemName: rocket.onPad ? "checkmark" : "xmark")
+                                    .foregroundColor(rocket.onPad ? .green : .red)
+                                    .padding()
+                            }
+                            HStack {
+                                Button(action: {
+                                    bluetoothDevice.setUtilities(input: "Bypass TVC")
+                                }) {
+                                    Text("TVC Active")
+                                }
                                 .padding()
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                                .foregroundColor(.black)
+                                .buttonStyle(.borderless)
+                                Image(systemName: rocket.tvcActive ? "checkmark" : "xmark")
+                                    .foregroundColor(rocket.tvcActive ? .green : .red)
+                                    .padding()
+                            }
+                            HStack {
+                                Button(action: {
+                                    bluetoothDevice.setUtilities(input: "Bypass Coasting")
+                                }) {
+                                    Text("Coasting")
+                                }
+                                .padding()
+                                .buttonStyle(.borderless)
+                                Image(systemName: rocket.coasting ? "checkmark" : "xmark")
+                                    .foregroundColor(rocket.coasting ? .green : .red)
+                                    .padding()
+                            }
+                            HStack {
+                                Button(action: {
+                                    bluetoothDevice.setUtilities(input: "Bypass Parachute")
+                                }) {
+                                    Text("Parachute Out")
+                                }
+                                .padding()
+                                .buttonStyle(.borderless)
+                                Image(systemName: rocket.parachuteOut ? "checkmark" : "xmark")
+                                    .foregroundColor(rocket.parachuteOut ? .green : .red)
+                                    .padding()
+                            }
+                            HStack {
+                                Button(action: {
+                                }) {
+                                    Text("Touchdown")
+                                }
+                                .padding()
+                                .buttonStyle(.borderless)
+                                Image(systemName: rocket.touchdown ? "checkmark" : "xmark")
+                                    .foregroundColor(rocket.touchdown ? .green : .red)
+                                    .padding()
+                            }
                         }
-                        Spacer()
-                        Image(systemName: "chevron.forward")
-                            .padding()
+                        Divider()
+                        Section {
+                            HStack {
+                                Text(rocket.logs)
+                                    .padding()
+                                    .font(.title3)
+                                Spacer()
+                                Button(action: {
+                                    rocket.clearLogs()
+                                }) {
+                                    Text("Clear Logs")
+                                        .font(.title3)
+                                        .buttonStyle(.borderless)
+                                        .padding()
+                                }
+                            }
+                        }
+                        
                     }
                 }
-                Divider()
-                HStack {
-                    NavigationLink {
-                        AltimeterView()
-                    } label: {
-                        Text("View Altimeter:")
-                            .padding()
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                            .foregroundColor(.black)
-                    }
-                    Spacer()
-                    Image(systemName: "chevron.forward")
-                        .padding()
-                }
-                Divider()
-                HStack {
-                    NavigationLink {
-                        ServoView()
-                    } label: {
-                        Text("View Servos:")
-                            .padding()
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                            .foregroundColor(.black)
-                    }
-                    Spacer()
-                    Image(systemName: "chevron.forward")
-                        .padding()
-                }
-                Divider()
-                HStack {
-                    NavigationLink {
-                        PIDView()
-                    } label: {
-                        Text("View PID:")
-                            .padding()
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                            .foregroundColor(.black)
-                    }
-                    Spacer()
-                    Image(systemName: "chevron.forward")
-                        .padding()
-                }
-                Divider()
-            }
-            Section {
-                HStack {
-                    Button(action: {
-                        bluetoothDevice.setUtilities(input: "Bypass Pad")
-                    }) {
-                        Text("On Pad")
-                    }
-                    .padding()
-                    .buttonStyle(.borderless)
-                    Image(systemName: rocket.onPad ? "checkmark" : "xmark")
-                        .foregroundColor(rocket.onPad ? .green : .red)
-                        .padding()
-                }
-                HStack {
-                    Button(action: {
-                        bluetoothDevice.setUtilities(input: "Bypass TVC")
-                    }) {
-                        Text("TVC Active")
-                    }
-                    .padding()
-                    .buttonStyle(.borderless)
-                    Image(systemName: rocket.tvcActive ? "checkmark" : "xmark")
-                        .foregroundColor(rocket.tvcActive ? .green : .red)
-                        .padding()
-                }
-                HStack {
-                    Button(action: {
-                        bluetoothDevice.setUtilities(input: "Bypass Coasting")
-                    }) {
-                        Text("Coasting")
-                    }
-                    .padding()
-                    .buttonStyle(.borderless)
-                    Image(systemName: rocket.coasting ? "checkmark" : "xmark")
-                        .foregroundColor(rocket.coasting ? .green : .red)
-                        .padding()
-                }
-                HStack {
-                    Button(action: {
-                        bluetoothDevice.setUtilities(input: "Bypass Parachute")
-                    }) {
-                        Text("Parachute Out")
-                    }
-                    .padding()
-                    .buttonStyle(.borderless)
-                    Image(systemName: rocket.parachuteOut ? "checkmark" : "xmark")
-                        .foregroundColor(rocket.parachuteOut ? .green : .red)
-                        .padding()
-                }
-                HStack {
-                    Button(action: {
-                    }) {
-                        Text("Touchdown")
-                    }
-                    .padding()
-                    .buttonStyle(.borderless)
-                    Image(systemName: rocket.touchdown ? "checkmark" : "xmark")
-                        .foregroundColor(rocket.touchdown ? .green : .red)
-                        .padding()
-                }
-            }
-            Divider()
-            Section {
-                HStack {
-                    Text(rocket.logs)
-                        .padding()
-                        .font(.title3)
-                    Spacer()
-                    Button(action: {
-                        rocket.clearLogs()
-                    }) {
-                        Text("Clear Logs")
-                            .font(.title3)
-                            .buttonStyle(.borderless)
-                            .padding()
-                    }
-                }
-            }
+                }.navigationBarTitle("TVC Control Panel")
             
-        }.navigationViewStyle(StackNavigationViewStyle())
-            .onDisappear(perform: {
-                bluetoothDevice.setUtilities(input: "Time Stop")
-            })
+        }
+        .onDisappear(perform: {
+            bluetoothDevice.setUtilities(input: "Time Stop")
+        })
+        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         HomeScreenView()
+            .environmentObject(BluetoothDeviceHelper())
+            .environmentObject(Rocket())
     }
 }
 
@@ -237,7 +164,7 @@ extension View {
     func hideKeyboardWhenTappedAround() -> some View  {
         return self.onTapGesture {
             UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder),
-                  to: nil, from: nil, for: nil)
+                                            to: nil, from: nil, for: nil)
         }
     }
 }
